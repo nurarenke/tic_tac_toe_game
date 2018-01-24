89114850d2aab80075c3b92c8c9fa2f948e4f6ec
Nura Renke <nurarenke@gmail.com>
2018-01-23 21:43:20 -0800

    keep a history of moves

--- src/index.js
+++ src/index.js
@@ -15,52 +15,20 @@ function Square(props){
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    {/* Constructor subclass to set it's inital state to contain an array
-    with 9 nulls, corresponding to the 9 squares */}
-    super(props);
-    this.state = {
-      squares:Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    {/* return and ignore click if someone won or the square is filled in */}
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-
-  }
 
   renderSquare(i) {
     {/*Passing down two props from Board to Square*/}
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)} 
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)} 
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -82,15 +50,79 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    {/* Constructor to set it's inital state to contain an array
+    with 9 nulls, corresponding to the 9 squares */}
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null), 
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    {/* return and ignore click if someone won or the square is filled in */}
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+      });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+    });
+    
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -106,6 +138,7 @@ ReactDOM.render(
 
 {/* Helper function to check if anyone has won the game */}
 function calculateWinner(squares) {
+  {/* lines is all of the possible winning combinations */}
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -119,6 +152,10 @@ function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+    {/* if the squares at index a is filled in and
+    squares at index a is equal to squares at index b and
+    squares at index a is equal to squares at index c then 
+    we have a filled row with matching letters*/}
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
       return squares[a];
     }
