196057f9580e913ce9be4b98706c9db6f3f218a1
Nura Renke <nurarenke@gmail.com>
2018-01-23 17:36:18 -0800

    switching between O and X

--- src/index.js
+++ src/index.js
@@ -21,13 +21,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares:Array(9).fill(null),
-      xIsNext: true;
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
@@ -46,7 +46,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
